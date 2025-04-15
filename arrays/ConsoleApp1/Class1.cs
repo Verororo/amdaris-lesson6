64d25755267e7549482534a8e18995a066aa35dd
Vero <kostea29@gmail.com>
2025-04-15 20:17:06 +0300

    exercise added

---
+++ arrays/ConsoleApp1/Class1.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public abstract class Entity
+    {
+        public int Id { get; set; }
+    }
+
+    public class Book : Entity
+    {
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public string Isbn { get; set; }
+    }
+
+    public class UsersBook : Book
+    {
+        public enum State
+        {
+            Bad,
+            Moderate,
+            Good
+        }
+
+        public State BookState { get; set; }
+
+        public UsersBook(Book book, State state)
+        {
+            this.Id = book.Id;
+            this.Title = book.Title;
+            this.Author = book.Author;
+            this.Isbn = book.Isbn;
+            this.BookState = state;
+        }
+    }
+
+    public class User : Entity
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+    }
+
+    public interface IRepository<T> where T : Entity
+    {
+        T GetById(int id);
+        IList<T> FindAll();
+
+        void Add(T entity);
+        void Delete(T entity);
+        void Update(T entity);
+    }
+
+    public class UserBookRelation
+    {
+        private readonly IRepository<User> _userRepository;
+        private readonly IRepository<UsersBook> _wantedBookRepository;
+        private readonly IRepository<UsersBook> _givenOutBookRepository;
+        private readonly IRepository<Book> _availableBookRepository;
+
+        public UserBookRelation(IRepository<User> userRepository, IRepository<UsersBook> wantedBookRepository,
+            IRepository<UsersBook> givenOutBookRepository, IRepository<Book> availableBookRepository)
+        {
+            _userRepository = userRepository;
+            _wantedBookRepository = wantedBookRepository;
+            _givenOutBookRepository = givenOutBookRepository;
+            _availableBookRepository = availableBookRepository;
+        }
+
+        public bool AddBookToWanted(int userId, int bookId, UsersBook.State state)
+        {
+            User user = _userRepository.GetById(userId);
+            Book book = _availableBookRepository.GetById(bookId);
+
+            if (user == null || book == null || _givenOutBookRepository.GetById(bookId) != null)
+            {
+                return false;
+            }
+
+            UsersBook newBook = new UsersBook(book, state);
+            _wantedBookRepository.Add(newBook);
+
+            return true;
+        }
+    }
+}
---
+++ arrays/ConsoleApp1/Program.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using ConsoleApp1;
+
+public class InMemoryRepository<T> : IRepository<T> where T : Entity
+{
+    private readonly List<T> _entities = new List<T>();
+    private int _nextId = 1;
+
+    public T GetById(int id)
+    {
+        return _entities.FirstOrDefault(e => e.Id == id);
+    }
+
+    public IList<T> FindAll()
+    {
+        return _entities.ToList();
+    }
+
+    public void Add(T entity)
+    {
+        if (entity.Id == 0)
+        {
+            entity.Id = _nextId++;
+        }
+        _entities.Add(entity);
+    }
+
+    public void Delete(T entity)
+    {
+        _entities.RemoveAll(e => e.Id == entity.Id);
+    }
+
+    public void Update(T entity)
+    {
+        var existingEntity = GetById(entity.Id);
+        if (existingEntity != null)
+        {
+            _entities.Remove(existingEntity);
+            _entities.Add(entity);
+        }
+    }
+}
+
+public class Program
+{
+    public static void Main()
+    {
+        var userRepository = new InMemoryRepository<User>();
+        var wantedBookRepository = new InMemoryRepository<UsersBook>();
+        var givenOutBookRepository = new InMemoryRepository<UsersBook>();
+        var availableBookRepository = new InMemoryRepository<Book>();
+
+        var bookRelationService = new UserBookRelation(
+            userRepository,
+            wantedBookRepository,
+            givenOutBookRepository,
+            availableBookRepository
+        );
+
+        var user1 = new User { Name = "Vasily Pupkin", Email = "vasya@example.com" };
+        userRepository.Add(user1);
+
+        var book1 = new Book
+        {
+            Title = "Clean Code",
+            Author = "Robert C. Martin",
+            Isbn = "9780132350884"
+        };
+        availableBookRepository.Add(book1);
+
+        var book2 = new Book
+        {
+            Title = "Design Patterns",
+            Author = "Erich Gamma et al.",
+            Isbn = "9780201633610"
+        };
+        availableBookRepository.Add(book2);
+
+        bool success = bookRelationService.AddBookToWanted(user1.Id, book1.Id, UsersBook.State.Good);
+
+        if (success)
+        {
+            Console.WriteLine($"Book '{book1.Title}' has been added to {user1.Name}'s wanted list.");
+            
+            Console.WriteLine("\nWanted Books:");
+            var wantedBooks = wantedBookRepository.FindAll();
+            foreach (var book in wantedBooks)
+            {
+                Console.WriteLine($"- {book.Title} by {book.Author} (State: {book.BookState})");
+            }
+        }
+        else
+        {
+            Console.WriteLine("Failed to add book to wanted list.");
+        }
+
+        success = bookRelationService.AddBookToWanted(user1.Id, book2.Id, UsersBook.State.Moderate);
+
+        if (success)
+        {
+            Console.WriteLine($"Book '{book2.Title}' has been added to {user1.Name}'s wanted list.");
+
+            Console.WriteLine("\nWanted Books:");
+            var wantedBooks = wantedBookRepository.FindAll();
+            foreach (var book in wantedBooks)
+            {
+                Console.WriteLine($"- {book.Title} by {book.Author} (State: {book.BookState})");
+            }
+        }
+        else
+        {
+            Console.WriteLine("Failed to add book to wanted list.");
+        }
+    }
+}
